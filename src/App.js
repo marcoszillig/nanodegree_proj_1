bf89bfbdba65be4642ff425218fbc1523876abac
Marcos Zillig <marcos.zillig@justdigital.com.br>
2018-11-10 22:12:42 -0200

    Final do proj Contactos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 import * as ContactsAPI from './utils/ContactsAPI'
 import CreateContact from './CreateContact'
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   state = {
-    contacts: [],
-    screen: 'list'
+    contacts: []
   }
   componentDidMount() {
     ContactsAPI.getAll()
@@ -25,20 +25,32 @@ class App extends Component {
 
     ContactsAPI.remove(contact)
   }
+  createContact = (contact) => {
+    ContactsAPI.create(contact)
+      .then((contact) => {
+        this.setState((currentState) =>({
+          contacts: currentState.contacts.concat([contact])
+        }))
+      })
+  }
   render() {
     return (
       <div>
-        {this.state.screen === 'list' && (<ListContacts
-          contacts={this.state.contacts}
-          onDeleteContact={this.removeContact}
-          onNavigate={() => {
-            this.setState(() => ({
-              screen: 'create'
-            }))
-          }}
-        />
-        )}
-        {this.state.screen === 'create' && (<CreateContact />)}
+        <Route exact path='/' render={() => (
+          <ListContacts
+            contacts={this.state.contacts}
+            onDeleteContact={this.removeContact}
+          />
+        )} />
+        <Route path='/create' render={({ history }) => (
+          <CreateContact 
+            onCreateContact={(contact) => {
+              this.createContact(contact)
+              history.push('/')
+            }}
+          />
+        
+        )} />
       </div>
     )
   }
